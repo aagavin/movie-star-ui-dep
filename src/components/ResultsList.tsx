a736d11ca06ff5b37866174707df0ea2bc9a9c5a
aagavin <aagavin@gmail.com>
2019-07-18 21:58:51 -0400

    fixed home page not being found

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -6,7 +6,7 @@ import { BASE_IMG } from "../declarations";
 const ResultsList: React.FunctionComponent<any> = (props: any) => (
   <IonList>
     {props.results.map((result: any) => (
-      <IonItem detail button key={result.id} onClick={e => { e.preventDefault(); props.history.push(`/home/media/${result.media_type}/${result.id}`) }}>
+      <IonItem detail button key={result.id} onClick={e => { e.preventDefault(); props.history.push(`/home/media/${result.media_type || props.catogery}/${result.id}`) }}>
         <IonThumbnail slot="start">
           <IonImg src={`${BASE_IMG}/w45${result.poster_path}`} />
         </IonThumbnail>
