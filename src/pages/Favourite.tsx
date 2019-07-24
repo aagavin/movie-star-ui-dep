e90f7b8dce7cc3184c561f7b1119d1139cfc70b2
aagavin <aagavin@gmail.com>
2019-07-23 20:33:01 -0400

    show favs

--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { auth } from 'firebase';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle } from '@ionic/react';
-
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonProgressBar } from '@ionic/react';
 import withFirebaseAuth from 'react-with-firebase-auth'
-import { providers, firebaseAppAuth } from "../firebaseConfig";
+
+import MediaDetailsCard from '../components/MediaDetailsCard';
+
+import { BASE_URL, MediaDetail } from '../declarations';
+import { providers, firebaseAppAuth } from '../firebaseConfig';
+
+interface FavResults {
+  favs?: MediaDetail[]
+}
 
 const Favourite: React.FC<any> = props => {
 
+  const [results, setResults] = useState<FavResults>();
+
   useEffect(() => {
     if (props.user) {
-      auth().currentUser.getIdToken(true).then(console.log)
+      auth().currentUser.getIdToken(true).then(async (token) => {
+        const url = `${BASE_URL}/user/favourites`;
+        const favs = await fetch(url, { headers: { 'id_token': token } }).then(r => r.json());
+        setResults(favs);
+        console.log(favs);
+      });
     }
   }, [props.user])
 
@@ -22,6 +36,12 @@ const Favourite: React.FC<any> = props => {
         <IonTitle>Favourites</IonTitle>
       </IonToolbar>
     </IonHeader>
+    <IonContent>
+      {results ?
+        results.favs.map(res => <MediaDetailsCard key={res.id} res={res} />) :
+        <IonProgressBar type="indeterminate" />
+      }
+    </IonContent>
   </>)
 }
 
