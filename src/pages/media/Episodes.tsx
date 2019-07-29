18cff562a416bae46c23793d82b9c5c65a2dbf68
aagavin <aagavin@gmail.com>
2019-07-28 23:09:23 -0400

    cleanup

--- src/pages/media/Episodes.tsx
+++ src/pages/media/Episodes.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { IonHeader, IonToolbar, IonButtons, IonTitle, IonContent, IonButton, IonIcon, IonCard, IonCardHeader, IonCardSubtitle, IonImg, IonCardTitle, IonCardContent, IonRow, IonCol, IonBadge } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonTitle, IonContent, IonCard, IonCardHeader, IonCardSubtitle, IonImg, IonCardTitle, IonCardContent, IonRow, IonCol, IonBadge, IonBackButton } from '@ionic/react';
 import useReactRouter from 'use-react-router';
 import { BASE_URL, BASE_IMG } from "../../declarations";
 
@@ -18,10 +18,10 @@ interface CardResults {
   air_date: React.ReactNode,
 }
 
-const EpisodesPage: React.FC<any> = props => {
+const EpisodesPage: React.FC<any> = () => {
 
   const [results, setResults] = useState([]);
-  const { history, match } = useReactRouter();
+  const { match } = useReactRouter();
 
   useEffect(() => {
     match.params && getResults(match.params)
@@ -29,8 +29,8 @@ const EpisodesPage: React.FC<any> = props => {
 
   const getResults = async (locationState: LocationState) => {
     const fetchList = [];
-    for (let i = 1; i < locationState.numOfEpisodes; i++) {
-      const url = `${BASE_URL}/media/tv/${locationState.mediaId}/season/${locationState.seasonNumber}/episode/${i}`;
+    for (let i = 0; i < locationState.numOfEpisodes; i++) {
+      const url = `${BASE_URL}/media/tv/${locationState.mediaId}/season/${locationState.seasonNumber}/episode/${i+1}`;
       fetchList.push(fetch(url).then(r => r.json()));
     }
     const results = await Promise.all(fetchList);
@@ -65,9 +65,7 @@ const EpisodesPage: React.FC<any> = props => {
       <IonHeader>
         <IonToolbar color="primary">
           <IonButtons slot="start">
-            <IonButton routerDirection="back" onClick={history.goBack}>
-              <IonIcon name="arrow-back"></IonIcon>
-            </IonButton>
+            <IonBackButton defaultHref={`/home/media/tv/${match.params['mediaId']}`} />
           </IonButtons>
           <IonTitle>Episodes</IonTitle>
         </IonToolbar>
