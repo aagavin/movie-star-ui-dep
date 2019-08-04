ebcf8fe008cb17a728af66b64227e7fb40ac43dc
aagavin <aagavin@gmail.com>
2019-08-03 20:44:25 -0400

    start convert to context

--- src/pages/account/Login.tsx
+++ src/pages/account/Login.tsx
@@ -1,8 +1,9 @@
 import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonMenuButton, IonTitle, IonToolbar, IonCardContent } from '@ionic/react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import useReactRouter from 'use-react-router';
 
-import withFirebaseAuth from 'react-with-firebase-auth'
+import withFirebaseAuth from 'react-with-firebase-auth';
+import UserContext from '../../context';
 import { providers, firebaseAppAuth } from "../../firebaseConfig";
 
 
@@ -11,12 +12,13 @@ const LoginPage: React.FC<any> = props => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const { history } = useReactRouter();
+  const context = useContext<any>(UserContext);
 
   useEffect(() => {
-    if (props.user) {
+    if (context.user) {
       history.replace('/');
     }
-  }, [props.user, history]);
+  }, [context.user, history]);
 
   const handleLogin = async () => props.signInWithEmailAndPassword(username, password);
 
@@ -32,7 +34,7 @@ const LoginPage: React.FC<any> = props => {
       </IonHeader>
       <IonContent>
         <IonCardContent>
-          {!props.user && (
+          {!context.user && (
             <>
               <IonItem>
                 <IonLabel position="floating">Email</IonLabel>
@@ -44,7 +46,7 @@ const LoginPage: React.FC<any> = props => {
               </IonItem>
               <IonButton expand="full" onClick={handleLogin}>Sign In</IonButton>
             </>)}
-          {(props.error && !props.user) && <p>{props.error}</p>}
+          {(props.error && !context.user) && <p>{props.error}</p>}
 
           <p>Don't have an account? <IonButton onClick={() => history.replace('/account/signup')}>Create one</IonButton></p>
         </IonCardContent>
