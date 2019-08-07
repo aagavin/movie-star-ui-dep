22a781191a6025fc91fe1f78b47592a78c28f95c
aagavin <aagavin@gmail.com>
2019-08-07 16:31:50 -0400

    used session storage to store favourites

--- src/context.tsx
+++ src/context.tsx
@@ -1,3 +1,46 @@
-import React from 'react';
+import { createContext } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/firestore';
 
-export default React.createContext({user: {}, favourites: [], signInWithEmailAndPassword: null, createUserWithEmailAndPassword: null, error: null});
+const addFavourite = async (uid: string, fav: {}) => {
+  return firebase
+    .firestore()
+    .collection('favs')
+    .doc(uid)
+    .set(fav, { merge: true });
+};
+
+const removeFavourite = async (uid: string, delId) => {
+  const delFav = {};
+  delFav[delId] = firebase.firestore.FieldValue.delete();
+  return firebase
+    .firestore()
+    .collection('favs')
+    .doc(uid)
+    .update(delFav);
+}
+
+const getFavourites = async (uid: string) => {
+  return firebase
+    .firestore()
+    .collection('favs')
+    .doc(uid)
+    .get();
+}
+
+const UserContext = createContext(
+  {
+    user: {},
+    signInWithEmailAndPassword: null,
+    createUserWithEmailAndPassword: null,
+    error: null,
+    addFavourite,
+    removeFavourite,
+    getFavourites
+  }
+);
+
+export const UserProvider = UserContext.Provider;
+export const UserConsumer = UserContext.Consumer;
+export default UserContext
--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonProgressBar } from '@ionic/react';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/firestore';
-import useReactRouter from 'use-react-router';
 
 import UserContext from '../context';
 import ResultsList from '../components/ResultsList';
@@ -19,27 +15,15 @@ interface FavResults {
 const Favourite: React.FC<any> = props => {
 
   const [results, setResults] = useState<FavResults[]>();
-  const { history } = useReactRouter();
   const context = useContext<any>(UserContext);
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    if (history.location.pathname !== '/favourite' && !history.location.pathname.includes('/home/media')) {
-      setResults([]);
-    }
-    else if (history.location.pathname === '/favourite') {
-      getFavs();
-    }
-  }, [history.location.pathname, context.user]);
+    const favs = sessionStorage.getItem('favs');
+    setResults(JSON.parse(favs));
+  }, [context, sessionStorage.getItem('favs')]);
   /* eslint-enable react-hooks/exhaustive-deps */
 
-  const getFavs = async () => {
-    if (context.user) {
-      firebase.firestore().collection('favs').doc(context.user.uid).get().then(doc => {
-        doc.data() ? setResults(Object.values(doc.data())) : setResults([]);
-      });
-    }
-  }
 
   const getContent = () => {
     if (context.user === null) {
