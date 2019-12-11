e8fcc2cea765375a060a1adf066cbca94989baee
aagavin <aagavin@gmail.com>
2019-12-11 12:10:29 -0500

    new service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { register as swregister } from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+swregister({
+  onUpdate: async (registration: ServiceWorkerRegistration) => {
+    await registration.update();
+  }
+});
