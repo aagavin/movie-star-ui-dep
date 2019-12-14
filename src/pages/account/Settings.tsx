32505bb6caa39a0edbbb9f9c1304bda91b261d90
aagavin <aagavin@gmail.com>
2019-12-14 13:09:31 -0500

    save settings to database

--- src/pages/account/Settings.tsx
+++ src/pages/account/Settings.tsx
@@ -1,13 +1,43 @@
 import { ToggleChangeEventDetail } from '@ionic/core/dist/types/components/toggle/toggle-interface';
-import { IonButtons, IonCardContent, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToggle,
+  IonToolbar
+} from '@ionic/react';
+import firebase, { firestore } from 'firebase/app';
+import 'firebase/database';
+import 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import UserContext from '../../context';
+import { UserSettings } from '../../declarations';
 
 const Settings: React.FC<any> = () => {
 
-  const setSettings = (e: CustomEvent<ToggleChangeEventDetail>) => {
+  const [settings, setSettings] = useState<UserSettings>({});
+  const context = useContext<any>(UserContext);
+  
+  useEffect(() => {
+    // tslint:disable-next-line: no-unused-expression
+    context.user && firebase.firestore().collection('settings').doc(context.user.uid).get().then(setDoc => {
+      setSettings(setDoc.data());
+    });
+  }, [context.user]);
+
+  const saveSettings = (e: CustomEvent<ToggleChangeEventDetail>) => {
     // TMP Disable 
     // tslint:disable-next-line: no-console
-    console.log(e.detail.checked);
+    console.log(e);
+    settings[e.detail.value] = e.detail.checked;
+    // tslint:disable-next-line: no-unused-expression
+    context.user && firestore().collection('settings').doc(context.user.uid).update(settings).then(() => console.log('updated'));
   };
 
   return (
@@ -21,14 +51,12 @@ const Settings: React.FC<any> = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonCardContent>
           <IonList>
-            <IonItem>
+            <IonItem button>
               <IonLabel>Public Favourites</IonLabel>
-              <IonToggle value="publicFav" onIonChange={setSettings} />
+              <IonToggle value="publicFav" checked={settings.publicFav} onIonChange={saveSettings} />
             </IonItem>
           </IonList>
-        </IonCardContent>
       </IonContent>
     </IonPage>
   )
