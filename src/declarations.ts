060a5188bffe8c71ff2daf9b3106ebefd0d7244d
Aaron Fernandes <afernandes@atb.com>
2019-12-27 10:55:45 -0500

    v2 init

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://watch-tv-list.herokuapp.com';
+export const BASE_URL = 'https://watch-tv-list.herokuapp.com/v2';
 export const BASE_IMG = 'https://image.tmdb.org/t/p';
 
 /***** AppPage *****/ 
