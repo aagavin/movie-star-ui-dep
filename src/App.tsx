ecdb60fbf1b7152cd8e03fb5b55a5c19153b3423
aagavin <aagavin@gmail.com>
2020-01-16 20:54:59 -0500

    experment with removeing css

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,12 @@ import '@ionic/core/css/structure.css';
 import '@ionic/core/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/core/css/display.css';
-import '@ionic/core/css/flex-utils.css';
-import '@ionic/core/css/float-elements.css';
-import '@ionic/core/css/padding.css';
-import '@ionic/core/css/text-alignment.css';
-import '@ionic/core/css/text-transformation.css';
+// import '@ionic/core/css/display.css';
+// import '@ionic/core/css/flex-utils.css';
+// import '@ionic/core/css/float-elements.css';
+// import '@ionic/core/css/padding.css';
+// import '@ionic/core/css/text-alignment.css';
+// import '@ionic/core/css/text-transformation.css';
 
 const commonPages: AppPage[] = [
   {
