c878de12ed48559a437bfbbb653dd33c5574c2fd
aagavin <aagavin@gmail.com>
2020-03-22 21:11:18 -0400

    test fix

--- src/__tests__/pages/account/Settings.test.tsx
+++ src/__tests__/pages/account/Settings.test.tsx
@@ -8,6 +8,26 @@ import UserContext, { init } from '../../../context';
 import Settings from '../../../pages/account/Settings';
 
 describe('Settings Page', () => {
+
+  beforeAll(() => {
+    jest.mock('firebase/app', () => ({
+      initializeApp: jest.fn(config => ({})),
+      firestore: jest.fn(() => ({
+        collection: jest.fn(collectionName => ({
+          doc: jest.fn(userID => ({
+            get: jest.fn(()=> Promise.resolve(() => ({
+              publicFav: true,
+              data: () => ({bla: 'test'})
+            })))
+          }))
+        }))
+      }))
+    }));
+
+    firebase.initializeApp({projectId: 'sdf'});
+
+  })
+
   afterEach(cleanup);
 
   const getUi = (rest = {}, history = createMemoryHistory()) => {
@@ -30,22 +50,7 @@ describe('Settings Page', () => {
 
   test('toggle public favourite ', async () => {
 
-    jest.mock('firebase/app', () => ({
-      initializeApp: jest.fn(config => ({})),
-      firestore: jest.fn(() => ({
-        collection: jest.fn(collectionName => ({
-          doc: jest.fn(userID => ({
-            get: jest.fn(()=> Promise.resolve(() => ({
-              publicFav: true
-            })))
-          }))
-        }))
-      }))
-    }));
-
-    firebase.initializeApp({projectId: 'sdf'});
-
-    const { container, debug } = getUi({user: {uid: '123456'}});
+    const { container } = getUi({user: {uid: '123456'}});
     const firstToggle = container.querySelectorAll('input[role="toggle"]');
     fireEvent.click(firstToggle[0]);
     expect(firstToggle.length).toBe(1);
