1b205ec59964ebcd15ad90fb86a0bbf4e2fb0817
aagavin <aagavin@gmail.com>
2020-04-12 10:08:24 -0400

    more server config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,11 +1,17 @@
 // jest-puppeteer.config.js
 module.exports = {
   server: {
-    command: 'SKIP_PREFLIGHT_CHECK=true ionic serve',
+    command: 'ionic serve --ci --no-open',
     port: 8100,
   },
   launch: {
     dumpio: false,
     headless: 'false',
+    defaultViewport: {
+      width: 415,
+      height: 730,
+      isMobile: true,
+      hasTouch: true
+    }
   }
 }
