8fc1d138a3ee442da2725e0b1c1fbeb2c3e8ff39
aagavin <aagavin@gmail.com>
2020-04-28 00:01:25 -0400

    fix a few bugs on episodes

--- src/components/MediaDetailsCard.tsx
+++ src/components/MediaDetailsCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { 
+import {
   IonCard,
   IonCardHeader,
   IonCardSubtitle,
@@ -21,35 +21,33 @@ import { withRouter } from 'react-router';
 
 const MediaDetailsCard: React.FC<any> = (props: any) => {
 
-  const [selectedSeason, setSelectedSeason] = useState('0');
-  
+  const [selectedSeason, setSelectedSeason] = useState(0);
+
   // TODO
   const getSeasonsCard = () => {
     if (props?.seasons.length > 0) {
 
       return (
         <IonCard>
-        <IonSegment scrollable value="heart" onIonChange={e => {
-          const s = e.detail.value.split('ion-sb-')[1];
-          if (s !== undefined) {
-            console.log(s);
-            setSelectedSeason(s)   
-          }
-       
-        } }>
-          {props.seasons.map(season => (
-            <IonSegmentButton>{season.season}</IonSegmentButton>
-          ))}
+          <IonSegment scrollable value="heart" onIonChange={e => {
+            if (!isNaN(parseInt(e.detail.value))) {
+              setSelectedSeason(parseInt(e.detail.value) - 1)
+            }
+          }}>
+            {props?.seasons.map(season => (
+              <IonSegmentButton key={season.season} value={season.season}>{season.season}</IonSegmentButton>
+            ))}
 
-        </IonSegment>
-        <IonList>
-        {(selectedSeason !== undefined) && props.seasons[parseInt(selectedSeason)].episodes.map(ep => (
-          <IonItem key={ep.id} id={ep.id} onClick={() => props.history.push(`/home/media/tvEpisode/${ep.id.split('/')[2]}`)} detail button>
-          {ep.title}
-        </IonItem>
-        ))}
-        
-        </IonList>
+          </IonSegment>
+          <IonList>
+            {(selectedSeason !== undefined) && props.seasons && props.seasons[selectedSeason].episodes.map(ep => (
+              <IonItem key={ep.id} id={ep.id} onClick={() => props.history.push(`/home/media/tvEpisode/${ep.id.split('/')[2]}`)} detail button>
+                {ep.title}
+              </IonItem>
+            ))
+            }
+
+          </IonList>
         </IonCard>
       )
     }
@@ -57,41 +55,41 @@ const MediaDetailsCard: React.FC<any> = (props: any) => {
   }
 
   return (
-  <>
-    <IonCard id={`card-${props.result.id}`}>
-      <IonCardHeader>
-        <IonCardSubtitle>
-          <IonImg src={props.result.image.url.replace('_V1_', `_SX${Math.floor(props.screenSize.width * .9)}_`)} alt={`poster for ${props.result.title}`} />
-        </IonCardSubtitle>
-        <IonCardTitle><b>{props.result.title}</b></IonCardTitle>
-      </IonCardHeader>
-      <IonCardContent>
-        {props.summary}
-        <IonGrid align-items-start>
-          <IonRow>
-            <IonCol size="auto">
-              <IonBadge color="light">{props.result.badge1}</IonBadge>
-            </IonCol>
-            <IonCol size="auto">
-              <IonBadge color="light">{props.result.badge2}</IonBadge>
-            </IonCol>
-            <IonCol size="auto">
-              <IonBadge color="light">{props.result?.badge3}</IonBadge>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+    <>
+      <IonCard id={`card-${props.result.id}`}>
+        <IonCardHeader>
+          <IonCardSubtitle>
+            <IonImg src={props.result.image.url.replace('_V1_', `_SX${Math.floor(props.screenSize.width * .9)}_`)} alt={`poster for ${props.result.title}`} />
+          </IonCardSubtitle>
+          <IonCardTitle><b>{props.result.title}</b></IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          {props.summary}
+          <IonGrid align-items-start>
+            <IonRow>
+              <IonCol size="auto">
+                <IonBadge color="light">{props.result.badge1}</IonBadge>
+              </IonCol>
+              <IonCol size="auto">
+                <IonBadge color="light">{props.result.badge2}</IonBadge>
+              </IonCol>
+              <IonCol size="auto">
+                <IonBadge color="light">{props.result?.badge3}</IonBadge>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
 
-        <IonGrid>
-          {props.isFav ?
-            <IonButton expand="block" color="danger" onClick={() => props.removeFromFavourite(props.result.id)} >Remove as favourite</IonButton> :
-            (props.context.user && <IonButton expand="block" color="primary" onClick={() => props.addToFavourite(props.result.id)}>Add to favourite</IonButton>)
-          }
-        </IonGrid>
-      </IonCardContent>
-    </IonCard>
-    {getSeasonsCard()}
-  </>
-);
+          <IonGrid>
+            {props.isFav ?
+              <IonButton expand="block" color="danger" onClick={() => props.removeFromFavourite(props.result.id)} >Remove as favourite</IonButton> :
+              (props.context.user && <IonButton expand="block" color="primary" onClick={() => props.addToFavourite(props.result.id)}>Add to favourite</IonButton>)
+            }
+          </IonGrid>
+        </IonCardContent>
+      </IonCard>
+      {getSeasonsCard()}
+    </>
+  );
 
 };
 
